34386ac8d0f263046f47ccc13ae3a3206296eda6
Jacob Aharon <jacob.aharon@unimelb.edu.au>
2023-06-02 08:07:51 +1000

    Fix rabbitmqadmin rm bug
    If the rabbitmqadmin file is non existent the rm command fails,
    so we are forcing the rm to succeed.
    
    Change-Id: I35328e42b7e41ffc635b8539cd77fbcbd4a7fec4

--- manifests/install/rabbitmqadmin.pp
+++ manifests/install/rabbitmqadmin.pp
@@ -48,7 +48,7 @@ class rabbitmq::install::rabbitmqadmin {
     if $facts['rabbitmq_version'] != $facts['rabbitmq_version_ctl']{
       Exec { 'remove_rabbitmqadmin_if_versions_do_not_match':
         path    => ['/bin','/usr/bin','/sbin','/usr/sbin'],
-        command => "rm ${rabbitmq::rabbitmq_home}/rabbitmqadmin",
+        command => "rm -f ${rabbitmq::rabbitmq_home}/rabbitmqadmin",
         notify  => Archive['rabbitmqadmin'],
       }
     }
